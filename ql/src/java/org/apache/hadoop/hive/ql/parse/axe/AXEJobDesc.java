5f8c0bbea6c56d8aa64851fa23225135a525e470
czkkkkkk <zekucai@gmail.com>
2018-11-16 16:06:20 +0800

    [Minor Fix] Change TableDesc to Table (#9)

--- ql/src/java/org/apache/hadoop/hive/ql/parse/axe/AXEJobDesc.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/axe/AXEJobDesc.java
@@ -1,6 +1,5 @@
 package org.apache.hadoop.hive.ql.parse.axe;
 
-import org.apache.avro.generic.GenericData;
 import org.apache.hadoop.hive.ql.exec.TableScanOperator;
 import org.apache.hadoop.hive.ql.metadata.Table;
 
@@ -8,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AXEJobDesc {
-    private List<AXETableDesc> srcTables;
+    private List<AXETable> srcTables;
     private List<AXETableScanOperator> tableScanOperators;
 
     public AXEJobDesc() {
@@ -17,7 +16,7 @@ public class AXEJobDesc {
     }
 
     public void addSrcTable(Table table) {
-        srcTables.add(new AXETableDesc(table.getTableName()));
+        srcTables.add(new AXETable(table.getTableName()));
     }
     public void addTableScanOperator(TableScanOperator op) {
         tableScanOperators.add(new AXETableScanOperator(op));
